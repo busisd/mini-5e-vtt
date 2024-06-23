bca1f699f6fc7526ab47901b9dee6580a471bfd5
Daniel Busis <busis.daniel@gmail.com>
2024-06-23 18:43:35 -0400

    Initial setup with webpack and TypeScript

---
+++ README.md
@@ -0,0 +1,5 @@
+A project built using webpack and TypeScript.
+
+For now, followed many of the steps in: https://webpack.js.org/guides/getting-started/.
+
+Eventually, this will be a mini-VTT (Virtual Table Top) for 5th edition D&D.
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/index.ts",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
