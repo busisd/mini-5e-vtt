8e37991fb3724fcdbb24ac8cc2a20b7c899cd8c9
Daniel Busis <busis.daniel@gmail.com>
2024-06-24 00:25:10 -0400

    Add basic types and actions

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import "./style.css";
 import Icon from "./icon.png";
+import "./types"
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = ["Hello", "webpack", 123, 456].join(" ");
-  element.classList.add("hello");
+  // element.classList.add("hello");
 
-  const myIcon = new Image();
-  myIcon.src = Icon;
+  // const myIcon = new Image();
+  // myIcon.src = Icon;
 
-  element.appendChild(myIcon);
+  // element.appendChild(myIcon);
 
   return element;
 }
