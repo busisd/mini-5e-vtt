8ff7a79b2aa84e41ac2eac6be5fb0c34e14abc95
Daniel Busis <busis.daniel@gmail.com>
2024-08-04 01:16:17 -0400

    Add clear button, clean up DiceRollerView

--- src/components/DiceRollerView.tsx
+++ src/components/DiceRollerView.tsx
@@ -1,5 +1,3 @@
-import random from "lodash/random";
-import uniqueId from "lodash/uniqueId";
 import React, { useCallback, useState } from "react";
 import DieSvg, { TextSvg } from "../svg/DieSvg";
 import {
@@ -7,107 +5,11 @@ import {
   evaluateDiceExpression,
   EvaluatedResultEntry,
 } from "../util/rollexpression/rollExpressionEvaluator";
-import { tokenizeDiceString } from "../util/rollexpression/rollExpressionTokenizer";
-import "./DiceRollerView.css";
 import { OperatorType } from "../util/rollexpression/rollExpressionParser";
-
-/*** Parsing logic ***/
-const diceRegex = /^[1-9]\d*d[1-9]\d*$/;
-const flatBonusRegex = /^[1-9]\d*$/;
-
-type DiceRollDefinition = {
-  dice: number[];
-  flatBonuses: number[];
-};
-
-type DiceRollResult = {
-  rolls: { result: number; sides: number }[];
-  flatBonuses: number[];
-  id: string;
-};
-
-const rollDice = ({
-  dice,
-  flatBonuses,
-}: DiceRollDefinition): DiceRollResult => {
-  const rolls = dice.map((sides) => ({
-    sides,
-    result: random(1, sides),
-  }));
-
-  return {
-    rolls,
-    flatBonuses,
-    id: uniqueId("roll"),
-  };
-};
-
-const parseDiceString = (diceStr: string): DiceRollDefinition => {
-  if (diceStr == null || diceStr.length === 0) {
-    throw new Error("Enter an expression");
-  }
-
-  const tokens = diceStr.split("+").map((token) => token.trim());
-
-  // const tokenizedTokens = tokenizeDiceString(diceStr);
-  // console.log("tokenizedTokens", tokenizedTokens);
-  // const parsedTokens = parseTokens(tokenizedTokens);
-  // console.log("parsedTokens", parsedTokens);
-  // const parsedExpression = parseRollExpression(parsedTokens);
-  // console.log("parsedExpression", parsedExpression);
-  // const evaluatedExpression = evaluateOperand(parsedExpression);
-  // console.log("evaluatedExpression", evaluatedExpression);
-
-  const dice: number[] = [];
-  const flatBonuses: number[] = [];
-  tokens.forEach((token) => {
-    if (diceRegex.test(token)) {
-      dice.push(...parseDiceToken(token));
-    } else if (flatBonusRegex.test(token)) {
-      flatBonuses.push(parseFlatBonusToken(token));
-    } else {
-      throw new Error(`Could not parse token: ${token}`);
-    }
-  });
-
-  return { dice, flatBonuses };
-};
-
-const parseDiceToken = (damageToken: string) => {
-  const [amountString, maxRollString] = damageToken.split("d");
-  const amount = parseInt(amountString);
-  const maxRoll = parseInt(maxRollString);
-
-  const dice: number[] = [];
-  for (let i = 0; i < amount; i++) {
-    dice.push(maxRoll);
-  }
-
-  return dice;
-};
-
-const parseFlatBonusToken = (damageToken: string) => {
-  return parseInt(damageToken);
-};
+import "./DiceRollerView.css";
 
 const getErrorMessage = (e: unknown) =>
   (e as Error)?.message ?? "Unknown error";
-
-const DiceRollResultDisplay = ({ result }: { result: DiceRollResult }) => {
-  return (
-    <>
-      {result.rolls.map((dieRoll, index) => (
-        <DieSvg
-          key={index}
-          sides={dieRoll.sides}
-          text={dieRoll.result}
-          color={index > 0 ? "lightgrey" : "black"}
-        />
-      ))}
-      <br />
-    </>
-  );
-};
 
 const isOperatorEntry = (resultEntry: EvaluatedResultEntry) =>
   "operator" in resultEntry;
@@ -199,11 +101,6 @@ const DiceRollerView = () => {
     setInputError(null);
 
     try {
-      console.log(tokenizeDiceString(rawInput));
-
-      // const parsedInput = parseDiceString(rawInput);
-      // const result = rollDice(parsedInput);
-
       const evaluatedExpression = evaluateDiceExpression(rawInput);
 
       setResults((currentResults) => [evaluatedExpression, ...currentResults]);
@@ -220,7 +117,8 @@ const DiceRollerView = () => {
       />{" "}
       {inputError && <span className="input-error"> {inputError} </span>}
       <br />
-      <button onClick={onSubmit}>Roll dice</button>
+      <button onClick={onSubmit}>Roll dice</button>{" "}
+      <button onClick={() => setResults([])}>Clear</button>
       <br />
       {results.map((result) => (
         // <DiceRollResultDisplay key={result.id} result={result} />
